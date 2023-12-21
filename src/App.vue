536fe61b5abc8e97be6e046d65a1f017008e4a0a
Bogdan Bulakh <95144713+bbulakh@users.noreply.github.com>
2023-12-21 17:24:19 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Input from './components/Input.vue'
+</script>
+
+<template>
+  <main>
+    <Input />
+  </main>
+</template>
+
---
+++ src/components/Input.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+   <div>
+        <input class="border px-2 py-2" v-model="text" type="text" placeholder="Введите текст">
+    </div>
+
+ 
+</template>
+
